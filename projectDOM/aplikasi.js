a2085ad81675243174b50409133c46817f62345a
alfiansyahrul <syahrul.alfian1@gmail.com>
2020-01-28 11:06:06 +0700

    update

--- projectDOM/aplikasi.js
+++ projectDOM/aplikasi.js
@@ -10,10 +10,40 @@ loadEventListeners();
 
 
 function loadEventListeners(){
+
+	document.addEventListener('DOMContentLoaded', ambilTugas);
+	
 	form.addEventListener('submit', addTask);
 	taskList.addEventListener('click', removeTask);
 	clearBtn.addEventListener('click', clearTasks);
 	filter.addEventListener('keyup', filterTasks);
+}
+
+function ambilTugas(){
+	let tasks;
+
+	if(localStorage.getItem('tasks')=== null){
+		tasks = [];
+	}else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+	tasks.forEach(function(task){
+		const li 		= document.createElement('li');
+
+		li.className	= 'collection-item';
+	
+		li.appendChild(document.createTextNode(task));
+	
+		const link		= document.createElement('a');
+	
+		link.className	= 'delete-item secondary-content';
+		link.innerHTML 	= '<i class="fa fa-remove"></i>';
+	
+		li.appendChild(link);
+	
+		taskList.appendChild(li);
+		
+	});
 }
 
 
@@ -38,9 +68,24 @@ function addTask(e){
 
 	taskList.appendChild(li);
 
+	simpanData(taskInput.value);
+
 	taskInput.value	= '';
 
 	e.preventDefault();
+}
+
+function simpanData(task){
+	let tasks;
+
+	if(localStorage.getItem('tasks')=== null){
+		tasks = [];
+	}else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+	tasks.push(task);
+
+	localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function removeTask(e){
@@ -49,12 +94,39 @@ function removeTask(e){
 			e.target.parentElement.parentElement.remove();
 		}
 	}
+
+	hapusDataLocal(e.target.parentElement.parentElement);
+
+}
+
+function hapusDataLocal(taskItem){
+	let tasks;
+
+	if(localStorage.getItem('tasks')=== null){
+		tasks = [];
+	}else {
+		tasks = JSON.parse(localStorage.getItem('tasks'));
+	}
+
+	tasks.forEach(function(task, index){
+		if(taskItem.textContent === task){
+			tasks.splice(index, 1);
+		}
+	});
+
+	localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 function clearTasks(){
 	while(taskList.firstChild){
 		taskList.removeChild(taskList.firstChild);
 	}
+
+	hapusDataSemua();
+}
+
+function hapusDataSemua(){
+	localStorage.clear();
 }
 
 function filterTasks(e){
